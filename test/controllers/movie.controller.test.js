75c338380f35a1fdf2d9d5036b1f66138be23759
Elisa Cutrin <elisacutrin@gmail.com>
2025-06-19 11:59:34 +0200

    Create Movie controller test

---
+++ test/controllers/movie.controller.test.js
@@ -0,0 +1,6 @@
+// add imports
+
+describe("Movie controller", () => {
+  it("should give an error if movie already exists", () => {});
+  it("should give a 200 if movie is correctly saved", () => {});
+});
