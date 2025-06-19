fa030fb3744145be5b92a1cdfa30f88c9f56a9b4
Elisa Cutrin <elisacutrin@gmail.com>
2025-06-19 12:02:36 +0200

    More tests for movie controller

--- test/controllers/movie.controller.test.js
+++ test/controllers/movie.controller.test.js
@@ -1,6 +1,19 @@
 // add imports
 
 describe("Movie controller", () => {
-  it("should give an error if movie already exists", () => {});
+  it("should give a 400 error if movie already exists", () => {
+    //Given
+
+    //When
+    const response = controller.postNewMovie(movie);
+    
+    //Then
+    expect(response.status).toEqual(400);
+    
+  });
+  
+  it("should give a 400 error if there is no title", () => {
+  });
+  
   it("should give a 200 if movie is correctly saved", () => {});
 });
