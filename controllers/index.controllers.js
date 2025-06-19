2958fcc42d105c8a75aa99287f7d183fdef807c1
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2025-06-19 12:23:16 +0200

    feat: movie controllers

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,41 +1,9 @@
-const Movie = require('../models/movies.model')
-
 const getHome = async (req, res) => {
     res.render('home', {
     })
 }
 
-const postNewMovie = async (req, res) => {
-    try {
-        const { title } = req.body;
-        const moviesTitles = await Movie.find({}, "title").lean();
-        const titlesArray = moviesTitles.map(movie => movie.title.toLowerCase());
-        if (titlesArray.includes(title.toLowerCase())){
-            return res.status(400).json({
-                error: "Esa peli ya existe"
-            })
-        }
-
-        if (!title) {
-            return res.status(400).json({
-                error: "Title is required"
-            });
-        }
-
-        await Movie.create({
-            title: req.body.title
-        });
-
-        return res.status(201).send('Peli añadida exitosamente');
-    } catch (error) {
-        console.error("Error creating movie:", error.message);
-        return res.status(500).json({
-            error: "Internal Server Error"
-        });
-    }
-}
 
 module.exports = {
-    getHome,
-    postNewMovie
+    getHome
 }
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router();
 
 const indexControllers = require('../controllers/index.controllers');
+const movieControllers = require('../controllers/movie.controllers')
 
 router.get('/', indexControllers.getHome);
-router.post('/new-movie', indexControllers.postNewMovie)
+router.post('/new-movie', movieControllers.postNewMovie)
 
 
 module.exports = router
