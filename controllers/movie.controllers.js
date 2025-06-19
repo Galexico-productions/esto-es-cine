73bc38021a53b7d0f8fef96463f4c5a3d0a4937f
Balam Castro <64442582+ekbalam11@users.noreply.github.com>
2025-06-19 14:25:21 +0200

    feat: jest testing for postNewMovie() case 1 passed

--- controllers/movie.controllers.js
+++ controllers/movie.controllers.js
@@ -4,19 +4,21 @@ const Movie = require('../models/movies.model')
 const postNewMovie = async (req, res) => {
     try {
         const { title } = req.body;
-        const moviesTitles = await Movie.find({}, "title").lean();
-        const titlesArray = moviesTitles.map(movie => movie.title.toLowerCase());
-        if (titlesArray.includes(title.toLowerCase())){
-            return res.status(400).json({
-                error: "Esa peli ya existe"
-            })
-        }
-
         if (!title) {
             return res.status(400).json({
                 error: "Title is required"
             });
         }
+        const moviesTitles = await Movie.find({}, "title").lean();
+
+        const titlesArray = moviesTitles.map(movie => movie.title.toLowerCase());
+        if (titlesArray.includes(title.toLowerCase())) {
+            return res.status(400).json({
+                error: "Esa peli ya existe"
+            })
+        }
+
+
 
         await Movie.create({
             title: req.body.title
--- test/controllers/movie.controller.test.js
+++ test/controllers/movie.controller.test.js
@@ -1,19 +1,39 @@
-// add imports
+const { postNewMovie } = require('/controllers/movie.controllers')
+const Movie = require('/models/movies.model')
+jest.mock('/models/movies.model.js')
 
-describe("Movie controller", () => {
-  it("should give a 400 error if movie already exists", () => {
-    //Given
 
-    //When
-    const response = controller.postNewMovie(movie);
-    
-    //Then
-    expect(response.status).toEqual(400);
-    
-  });
-  
-  it("should give a 400 error if there is no title", () => {
-  });
-  
-  it("should give a 200 if movie is correctly saved", () => {});
+ it("should give a 400 error if there is no title", async () => {
+  //Given
+  const req = {body : {title : ''}}
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn()
+  };
+
+  //When
+  await postNewMovie(req, res)
+
+  //Then
+  expect(res.status).toHaveBeenCalledWith(400);
+  expect(res.json).toHaveBeenCalledWith({ error: "Title is required"})
+
 });
+
+// describe("Movie controller", () => {
+//   it("should give a 400 error if movie already exists", () => {
+//     //Given
+
+//     //When
+//     const response = postNewMovie(req, res);
+
+//     //Then
+//     expect(response.status).toEqual(400);
+
+//   });
+
+ 
+
+
+//   it("should give a 200 if movie is correctly saved", () => { });
+// });
